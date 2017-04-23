e70782823d925c4d7ba0efb6b70dde071a1b0da1
Valentyn <valentynmail@gmail.com>
2017-04-23 23:03:22 +0300

    Static100

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -121,11 +121,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # My settings Django has to know wherу loging page is (for decorators)
 LOGIN_URL = '/users/login/'
