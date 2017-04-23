d6022d8db64fdfb3e38fafc54207b94c68d97f5d
Valentyn <valentynmail@gmail.com>
2017-04-23 23:20:09 +0300

    Changes4

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
-        'DIRS': [os.path.join(BASE_DIR, 'learning_log/templates')],
+       # 'DIRS': [os.path.join(BASE_DIR, 'learning_log/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
