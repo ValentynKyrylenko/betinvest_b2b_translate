d6d1fe93a4cc99c1cb18e8381ca0ddf9c354182d
Valentyn <valentynmail@gmail.com>
2017-04-24 00:27:51 +0300

    normal_heroku1111111111

--- learning_log/wsgi.py
+++ learning_log/wsgi.py
@@ -13,11 +13,11 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
 
-application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
